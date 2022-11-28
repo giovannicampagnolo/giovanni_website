5e630009d1a203a4d6777324160dfa528b591a6f
Giovanni Campagnolo - Developer <115693598+giovannicampagnolo@users.noreply.github.com>
2022-11-28 16:49:37 +0100

    chore: add dependency tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
+
+    < div className="App" >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p style={{ fontFamily: "Flare", letterSpacing: '5px' }}>
           GIOVANNI <span style={{ color: "#0FF3EBE3" }}>C</span>AMPAGNOLO WEBSITE
         </p>
+        <a style={{ fontFamily: "Flare", letterSpacing: '5px' }}>
+          Enter</a>,
       </header>
-    </div>
+    </div >
+
   );
 }
 
